51f30ab6e714c2af51d68b0591253545545e0e53
Erk Sampat <erksampat@169-231-123-114.wireless.ucsb.edu>
2023-04-04 10:03:04 -0700

    change int types to unsigned long types; use mod to shift capitalization correctly

--- welcome/code/caprot.cpp
+++ welcome/code/caprot.cpp
@@ -11,26 +11,24 @@ int main(int argc, char *argv[]) {
         return 1;
     }
     string inputString = "";
-    int shiftNum = stoi(argv[1]);
-    vector<int> capitalIndexes;
+    long shiftNum = stoi(argv[1]);
+    unsigned long i = 0;
+    vector<unsigned long> capitalIndexes;
     getline(cin, inputString);
-    if (shiftNum < 0) {
-        cout << inputString << endl;
-        return 0;
-    }
-    for (int i = 0; i < int(inputString.length()); i++) {
+    for (i = 0; i < inputString.length(); i++) {
         if (isupper(inputString[i])) {
             capitalIndexes.push_back(i);
         }
     }
-    for (int i = 0; i < int(capitalIndexes.size()); i++) {
+    for (i = 0; i < capitalIndexes.size(); i++) {
         inputString[capitalIndexes.at(i)] = tolower(inputString[capitalIndexes.at(i)]);
-        if (capitalIndexes.at(i) + shiftNum >= int(inputString.length())) {
+        /*if (capitalIndexes.at(i) + shiftNum >= inputString.length()) {
             inputString[capitalIndexes.at(i) + shiftNum - inputString.length() + 1] = toupper(inputString[capitalIndexes.at(i) + shiftNum - inputString.length() + 1]);
         }
         else {
             inputString[capitalIndexes.at(i) + shiftNum] = toupper(inputString[capitalIndexes.at(i) + shiftNum]);
-        }
+        }*/
+        inputString[(capitalIndexes.at(i) + shiftNum) % (inputString.length() - 1)] = toupper(inputString[(capitalIndexes.at(i) + shiftNum) % (inputString.length() - 1)]);
     }
     cout << inputString << endl;
     return 0;
