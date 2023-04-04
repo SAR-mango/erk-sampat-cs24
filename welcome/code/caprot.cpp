fe2d1fb8c3e90571b08dd2d52041dc5cd948302a
Erk Sampat <erksampat@169-231-123-114.wireless.ucsb.edu>
2023-04-04 10:52:01 -0700

    update algorithm for negative shiftNum values

--- welcome/code/caprot.cpp
+++ welcome/code/caprot.cpp
@@ -10,25 +10,40 @@ int main(int argc, char *argv[]) {
         cout << "Usage: ./caprot num_to_rotate" << endl;
         return 1;
     }
+
     string inputString = "";
     long shiftNum = stoi(argv[1]);
     unsigned long i = 0;
     vector<unsigned long> capitalIndexes;
     getline(cin, inputString);
+
     for (i = 0; i < inputString.length(); i++) {
         if (isupper(inputString[i])) {
             capitalIndexes.push_back(i);
             inputString[i] = tolower(inputString[i]);
         }
     }
-    for (i = 0; i < capitalIndexes.size(); i++) {
-        if (capitalIndexes.at(i) + shiftNum >= inputString.length()) {
-            inputString[(capitalIndexes.at(i) + shiftNum) % (inputString.length())] = toupper(inputString[(capitalIndexes.at(i) + shiftNum) % (inputString.length())]);
-        }
-        else {
-            inputString[capitalIndexes.at(i) + shiftNum] = toupper(inputString[capitalIndexes.at(i) + shiftNum]);
+    if (shiftNum >= 0) {
+        for (i = 0; i < capitalIndexes.size(); i++) {
+            if (capitalIndexes.at(i) + shiftNum >= inputString.length()) {
+                inputString[(capitalIndexes.at(i) + shiftNum) % (inputString.length())] = toupper(inputString[(capitalIndexes.at(i) + shiftNum) % (inputString.length())]);
+            }
+            else {
+                inputString[capitalIndexes.at(i) + shiftNum] = toupper(inputString[capitalIndexes.at(i) + shiftNum]);
+            }
         }
     }
+    else {
+        for (i = 0; i < capitalIndexes.size(); i++) {
+            if (capitalIndexes.at(i) + shiftNum < 0) {
+                inputString[inputString.length() - ((capitalIndexes.at(i) + shiftNum) % (inputString.length()))] = toupper(inputString[inputString.length() - ((capitalIndexes.at(i) + shiftNum) % (inputString.length()))]);
+            }
+            else {
+                inputString[capitalIndexes.at(i) + shiftNum] = toupper(inputString[capitalIndexes.at(i) + shiftNum]);
+            }
+        }
+    }
+
     cout << inputString << endl;
     return 0;
 }
