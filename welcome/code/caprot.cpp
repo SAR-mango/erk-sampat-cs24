d58058e436ab93202b8bc84afebe6745b13e0a8e
Erk Sampat <erksampat@169-231-123-114.wireless.ucsb.edu>
2023-04-04 11:09:31 -0700

    update caprot.cpp

--- welcome/code/caprot.cpp
+++ welcome/code/caprot.cpp
@@ -37,8 +37,8 @@ int main(int argc, char *argv[]) {
     else {
         for (i = 0; i < capitalIndexes.size(); i++) {
             if (capitalIndexes.at(i) + shiftNum < 0) {
-                inputString[inputString.length() - ((capitalIndexes.at(i) + shiftNum) % (inputString.length())) - 1] 
-                = toupper(inputString[inputString.length() - ((capitalIndexes.at(i) + shiftNum) % (inputString.length())) - 1]);
+                inputString[inputString.length() - ((capitalIndexes.at(i) + shiftNum) % (inputString.length()))] 
+                = toupper(inputString[inputString.length() - ((capitalIndexes.at(i) + shiftNum) % (inputString.length()))]);
             }
             else {
                 inputString[capitalIndexes.at(i) + shiftNum] = toupper(inputString[capitalIndexes.at(i) + shiftNum]);
