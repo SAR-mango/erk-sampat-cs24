6ea9dc87c8424816d42f6898b2b48deab465485c
Erk Sampat <erksampat@169-231-123-114.wireless.ucsb.edu>
2023-04-12 20:32:50 -0700

    create FibVec.h

--- fibvec/FibVec.h
+++ fibvec/FibVec.h
@@ -1,22 +1,27 @@
 #ifndef FIBVEC_H
 #define FIBVEC_H
 
-// This provides the size_t type:
 #include <cstddef>
 
 class FibVec {
-  // Member Variables
-
-  // Helper Functions
-
-public:
-  // Constructor and Destructor
-
-  // Member Functions
-
+    public:
+        FibVec();
+        ~FibVec();
+        size_t capacity () const;
+        size_t count () const;
+        void insert(int value, size_t index);
+        int lookup(size_t index) const;
+        int pop();
+        void push(int value);
+        int remove(size_t index);
+    private:
+        int* vector = NULL;
+        size_t fib(size_t n) const;
+        size_t curr_size = 1;
+        size_t curr_num_items = 0;
+        size_t n = 2;
 };
 
-// Used by the autograder:
 FibVec* create_fibvec();
 
 #endif
