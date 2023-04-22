95cd8fea6cb92252eece161e91ba79845edf8d46
Erk Sampat <erksampat@Erks-MacBook-Pro.local>
2023-04-21 20:29:58 -0700

    update parser

--- tictac/Move.cpp
+++ tictac/Move.cpp
@@ -89,6 +89,5 @@ void Move::error() {
 }*/
 
 std::ostream& operator << (std::ostream& stream, const Move& move) {
-    //std::cout << number << ' ' << player << ' ' << row << column << std::endl;
-    std::cout << number << ' ' << player << ' ' << row << column <<;
+    std::cout << move.number << ' ' << move.player << ' ' << move.row << move.column << std::endl;
 }
