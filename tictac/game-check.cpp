0c013500df223b5f833263b9dfbe5b402c0c6ced
Erk Sampat <erksampat@169-231-124-79.wireless.ucsb.edu>
2023-04-24 21:03:05 -0700

    implement game evaluator

--- tictac/game-check.cpp
+++ tictac/game-check.cpp
@@ -1,7 +1,61 @@
+#include <iostream>
+
 #include "Board.h"
 #include "Errors.h"
 #include "Move.h"
 
-int main() {
+int main (int argc, char** argv) {
+    bool verbose = false;
+    if (argc == 2 && std::string(argv[1]) == "-v") {
+        verbose = true;
+        std::cout << "> ";
+    }
 
+    std::string line;
+    Board board;
+    while (std::cin) {
+        try {
+            std::getline(std::cin, line);
+            Move move(line);
+            board.addMove(move);
+        }
+        catch(const ParseError& e) {
+            if (verbose) {
+                std::cout << "Parse error: " << e.what() << '\n';
+            }
+            else {
+                std::cout << "Parse error.\n";
+            }
+            return 1;
+        }
+        catch(const InvalidMove& e) {
+            if (verbose) {
+                std::cout << "Invalid move: " << e.what() << '\n';
+            }
+            else {
+                std::cout << "Invalid move.\n";
+            }
+            return 2;
+        }
+    }
+    Game_State status = board.getStatus();
+    if (status == GS_PROG_NEW) {
+        std::cout << "Game in progress: New game." << std::endl;
+    }
+    else if (status == GS_PROG_X_TURN) {
+        std::cout << "Game in progress: X's turn." << std::endl;
+    }
+    else if (status == GS_PROG_O_TURN) {
+        std::cout << "Game in progress: O's turn." << std::endl;
+    }
+    else if (status == GS_OVER_X_WIN) {
+        std::cout << "Game over: X wins." << std::endl;
+    }
+    else if (status == GS_OVER_O_WIN) {
+        std::cout << "Game over: O wins." << std::endl;
+    }
+    else {
+        std::cout << "Game over: Draw." << std::endl;
+    }
+    return 0;
 }
