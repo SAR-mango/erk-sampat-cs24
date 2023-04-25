dc05c6d48e1c15b0b350f6992735904d0dd96ca9
Erk Sampat <erksampat@169-231-124-79.wireless.ucsb.edu>
2023-04-24 17:31:31 -0700

    clean up Move.cpp

--- tictac/Move.cpp
+++ tictac/Move.cpp
@@ -6,10 +6,14 @@
 // Space for implementing Move functions.
 
 Move::Move(const std::string& input) {
+    // check for valid length of entry
     if (input.length() < 6) { // minimum length of a valid entry is 6
         throw ParseError("length less than minimum of 6");
     }
+
     size_t index = 0;
+
+    // parse move number
     if (isdigit(input[index])) {
         int move_num = std::atoi(&input[index]);
         if (1 <= move_num && move_num <= 9) {
@@ -22,7 +26,10 @@ Move::Move(const std::string& input) {
     else {
         throw ParseError("first character is not a number");
     }
+
     index++;
+
+    // traverse whitespace
     while (isspace(input[index])) {
         index++;
     }
@@ -32,13 +39,18 @@ Move::Move(const std::string& input) {
     if (index == 1) {
         throw ParseError("no whitespace following move number");
     }
+
+    // parse player code
     if (tolower(input[index]) == 'x' || tolower(input[index]) == 'o') {
         player = toupper(input[index]);
     }
     else {
         throw ParseError("invalid player code");
     }
+
     index++;
+
+    // traverse whitespace
     size_t temp_index = index;
     while (isspace(input[index])) {
         index++;
@@ -49,16 +61,23 @@ Move::Move(const std::string& input) {
     if (index == temp_index) {
         throw ParseError("no whitespace following player code");
     }
+
+    // parse row
     if (tolower(input[index]) == 'a' || tolower(input[index]) == 'b' || tolower(input[index]) == 'c') {
         row = toupper(input[index]);
     }
     else {
         throw ParseError("invalid row");
     }
+
     index++;
+
+    // prevent accessing data outside of string
     if (index >= input.length()) {
         throw ParseError("end of string reached prematurely");
     }
+
+    // parse column
     if (isdigit(input[index])) {
         int col = std::atoi(&input[index]);
         if (col == 1 || col == 2 || col == 3) {
@@ -71,7 +90,10 @@ Move::Move(const std::string& input) {
     else {
         throw ParseError("column is not a number");
     }
+
     index++;
+
+    // parse comments
     if (index < input.length()) {
         if (!isspace(input[index])) {
             throw ParseError("no whitespace following column");
