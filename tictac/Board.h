c39b70a882b15e28a8b4833437341ef846e273a4
Erk Sampat <erksampat@169-231-124-79.wireless.ucsb.edu>
2023-04-24 17:35:57 -0700

    add status variable initialized to GS_PROG_NEW

--- tictac/Board.h
+++ tictac/Board.h
@@ -18,6 +18,7 @@ class Board {
         int last_move_num = 0;
         char last_player = '';
         Square A1, A2, A3, B1, B2, B3, C1, C2, C3;
+        Game_State status = GS_PROG_NEW;
 
         bool checkWin();
         Square& getSquare(char row, int col);
