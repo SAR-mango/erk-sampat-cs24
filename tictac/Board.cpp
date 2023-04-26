6cf5f7e856a26c8bf59f7b44d09c290aa9c62620
Erk Sampat <erksampat@169-231-124-79.wireless.ucsb.edu>
2023-04-25 20:51:41 -0700

    fix bug in win checker

--- tictac/Board.cpp
+++ tictac/Board.cpp
@@ -1,5 +1,6 @@
 #include "Errors.h"
 #include "Board.h"
+//#include <iostream> // TODO remove
 
 void Board::addMove(Move move) {
     if (last_move_num == 0) {
@@ -39,7 +40,7 @@ void Board::addMove(Move move) {
             status = GS_PROG_X_TURN;
         }
         getSquare(move.row, move.column)->occupier = move.player;
-        if (last_move_num >= 3) {
+        if (last_move_num >= 5) {
             if (getWinner() == 'X') {
                 status = GS_OVER_X_W;
             }
@@ -61,7 +62,9 @@ Game_State Board::getStatus() {
 
 char Board::getWinner() {
     for (int i = 0; i < 8; i++) {
-        if (wins[i][0]->occupier == wins[i][1]->occupier && wins[i][0]->occupier == wins[i][2]->occupier) {
+        //std::cout << i << ' ' << wins[i][0]->occupier << wins[i][1]->occupier << wins[i][2]->occupier << std::endl;
+        if (wins[i][0]->occupier == wins[i][1]->occupier && wins[i][0]->occupier == wins[i][2]->occupier && (wins[i][0]->occupier == 'X' || wins[i][0]->occupier == 'O')) {
+            //std::cout << "done" << std::endl;
             return wins[i][0]->occupier;
         }
     }
