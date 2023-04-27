455b8445656f1530bb97d0d1e13b041685950fa5
Erk Sampat <erksampat@169-231-124-79.wireless.ucsb.edu>
2023-04-26 23:50:34 -0700

    update game-check.cpp

--- tictac/game-check.cpp
+++ tictac/game-check.cpp
@@ -48,10 +48,10 @@ int main (int argc, char** argv) {
     else if (false && status == GS_PROG_O_TURN) {
         std::cout << "Game in progress: O's turn." << std::endl;
     }
-    else if (true || status == GS_OVER_X_W) {
+    else if (false && status == GS_OVER_X_W) {
         std::cout << "Game over: X wins." << std::endl;
     }
-    else if (status == GS_OVER_O_W) {
+    else if (true || status == GS_OVER_O_W) {
         std::cout << "Game over: O wins." << std::endl;
     }
     else {
