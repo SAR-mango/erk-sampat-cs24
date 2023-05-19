70d2973c4884bc100c31c4805dc02648a62d70cf
Erk Sampat <erksampat@169-231-122-114.wireless.ucsb.edu>
2023-05-19 11:33:00 -0700

    remove commented code

--- counter/Index.cpp
+++ counter/Index.cpp
@@ -12,23 +12,6 @@ Index::~Index() {
 }
 
 Node* Index::keyToNode(const std::string& key, size_t& index) const {
-    /*size_t length = key.length();
-    if (length == 0) {
-        index = 0;
-        return indices[0];
-    }
-    else if (length == 1) {
-        index = key[0] % (size - 1);
-        return indices[index];
-    }
-    else if (length == 2) {
-        index = (key[0] + key[1]) % (size - 1);
-        return indices[index];
-    }
-    else {
-        index = (key[0] + key[1] + key[2]) % (size - 1);
-        return indices[index];
-    }*/
     const char* str = key.c_str();
     long hashVal = 0;
 	while (*str != '\0') {
