d67e068a7654a1beb821ff12069f3ec9407f6b5e
Erk Sampat <erksampat@169-231-122-114.wireless.ucsb.edu>
2023-05-24 18:05:32 -0700

    update push

--- typo/Heap.cpp
+++ typo/Heap.cpp
@@ -1,41 +1,50 @@
 #include "Heap.h"
 #include <stdexcept>
+#include <iostream>
 
 Heap::Heap(size_t capacity) {
-    mCapacity = capacity;
-    mCount = 0;
-    if (mCapacity == 0) {
-        mData = nullptr;
+    if (capacity > 0) {
+        mCapacity = capacity;
+        mData = new Entry[capacity];
+        mCount = 0;
     }
     else {
-        mData = new Entry[mCapacity];
+        mCapacity = 0;
+        mData = nullptr;
+        mCount = 0;
     }
 }
 
 Heap::Heap(const Heap& other) {
-    mCapacity = other.mCapacity;
-    mCount = other.mCount;
-    if (mCapacity == 0) {
-        mData = nullptr;
-    }
-    else {
+    if (other.mCapacity > 0) {
+        mCapacity = other.mCapacity;
         mData = new Entry[mCapacity];
-        for (size_t i = 0; i < mCapacity; i++) {
+        mCount = other.mCount;
+        for (size_t i = 0; i < mCount; i++) {
             mData[i] = other.mData[i];
         }
+    }
+    else {
+        mCapacity = 0;
+        mData = nullptr;
+        mCount = 0;
     }
 }
 
 Heap::Heap(Heap&& other) {
+    mCapacity = other.mCapacity;
+    other.mCapacity = 0;
     mData = other.mData;
     other.mData = nullptr;
+    mCount = other.mCount;
+    other.mCount = 0;
 }
 
 Heap::~Heap() {
-    delete [] mData;
-    mCount = 0;
     mCapacity = 0;
+    delete [] mData;
     mData = nullptr;
+    mCount = 0;
 }
 
 size_t Heap::capacity() const {
@@ -54,83 +63,46 @@ const Heap::Entry& Heap::lookup(size_t index) const {
 }
 
 Heap::Entry Heap::pop() {
-    if (mCount == 0) {
-        throw std::underflow_error("no items in heap");
-    }
-    Entry popped = mData[0];
-    mData[0] = mData[mCount - 1];
-    //delete mData[mCount - 1];
-    size_t curr_index = 0;
-    size_t lc_index = curr_index * 2 + 1;
-    size_t rc_index = curr_index * 2 + 2;
-    Entry curr = mData[curr_index];
-    Entry lc = mData[lc_index];
-    Entry rc = mData[rc_index];
-    while (lc.score < curr.score || rc.score < curr.score) {
-        if (lc.score <= rc.score) {
-            Entry temp = lc;
-            mData[lc_index] = curr;
-            mData[curr_index] = temp;
-            curr_index = lc_index;
-            lc_index = curr_index * 2 + 1;
-            rc_index = curr_index * 2 + 2;
-            curr = mData[curr_index];
-            lc = mData[lc_index];
-            rc = mData[rc_index];
-        }
-        else {
-            Entry temp = rc;
-            mData[rc_index] = curr;
-            mData[curr_index] = temp;
-            curr_index = rc_index;
-            lc_index = curr_index * 2 + 1;
-            rc_index = curr_index * 2 + 2;
-            curr = mData[curr_index];
-            lc = mData[lc_index];
-            rc = mData[rc_index];
-        }
-    }
-    mCount--;
-    return popped;
 }
 
 Heap::Entry Heap::pushpop(const std::string& value, float score) {
-    Entry entry;
-    return entry;
 }
 
 void Heap::push(const std::string& value, float score) {
     if (mCount == mCapacity) {
         throw std::overflow_error("heap is full");
     }
-    Entry new_entry;
-    new_entry.value = value;
-    new_entry.score = score;
-    mData[mCount] = new_entry;
     if (mCount == 0) {
+        //std::cout << "nothing in heap, so putting " << value << " in index 0" << std::endl;
+        mData[0].value = value;
+        mData[0].score = score;
         mCount++;
         return;
     }
-    size_t parent_index = (mCount - 1) / 2;
-    size_t new_index = mCount;
-    Entry parent = mData[parent_index];
-    while (parent.score > new_entry.score || new_index > 0) {
-        Entry temp = parent;
-        mData[parent_index] = new_entry;
-        mData[new_index] = temp;
-        parent = temp;
-        new_index = parent_index;
-        parent_index = (new_index - 1) / 2;
-        parent = mData[parent_index];
+    mData[mCount].value = value;
+    mData[mCount].score = score; // put new entry in first available slot
+    //std::cout << "heap not empty, so putting " << value << " in index " << mCount << std::endl;
+    size_t i = mCount; // index of new entry
+    Entry new_entry = mData[i]; // save a copy of new entry
+    Entry parent_entry = mData[(i - 1) / 2]; // save a copy of its parent
+    while (i != 0 && parent_entry.score > new_entry.score) { // if new entry is at the beginning or its score is higher than its parent's score, we are done; otherwise, swap the new entry with its parent
+        //std::cout << "swapping" << std::endl;
+        mData[i] = parent_entry; // replace new entry with its parent
+        i = (i - 1) / 2; // index of new entry is now index of its parent
+        mData[i] = new_entry; // replace parent with new entry
+        if (i > 0) {
+            parent_entry = mData[(i - 1) / 2]; // set the new parent, but only if the new entry is not at the beginning
+        }
     }
     mCount++;
+    for (size_t i = 0; i < mCount; i++) {
+        //std::cout << '[' << mData[i].value << ", " << mData[i].score << "] ";
+    }
+    //std::cout << std::endl;
+    return;
 }
 
 const Heap::Entry& Heap::top() const {
-    if (mCount == 0) {
-        throw std::underflow_error("no items in heap");
-    }
-    return mData[0];
 }
 
 /*class Heap {
